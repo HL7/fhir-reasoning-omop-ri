586a7c91a1ee96cfd1541b0139b0d8ae9f2b8a1d
Dylan Phelan <dtphelan1@gmail.com>
2022-09-17 17:42:20 -0400

    improved cohort loading

--- webapi/cohort-initialize/loadCohorts.js
+++ webapi/cohort-initialize/loadCohorts.js
@@ -1,12 +1,31 @@
 const fs = require('fs');
 const path = require('path');
 const axios = require('axios');
+const {parse} = require('csv-parse/sync')
+const NAMELOOKUP  = 'cohortNameLookup.csv'
+
+// Get to-to-name lookup csv from disk 
+const lookupFilePath = path.resolve(__dirname, NAMELOOKUP)
+const lookupFile = fs.readFileSync(lookupFilePath)
+const lookupJson = parse(lookupFile, { 
+  columns: true,
+  skip_empty_lines: true
+})
 
 async function getCohort(fileName) {
   return fs.promises
     .readFile(path.resolve(__dirname, 'cohorts', fileName))
     .then((file) => {
-      axios.post('http://host.docker.internal:8080/WebAPI/cohortdefinition', JSON.parse(file))
+      // NOTE: Hardcoded trim the final 5 chars since we know all are JSON
+      const id = fileName.slice(0,-5)
+      const cohort = JSON.parse(file)
+      const payload = { 
+        id: id,
+        name: lookupJson.find((row) => row.cohortId === id) ? lookupJson.find((row) => row.cohortId === id).cohortName : '',
+        // expressionType: SIMPLE_EXPRESSION
+        expression: cohort, 
+      }
+      axios.post('http://localhost:8080/WebAPI/cohortdefinition', payload)
       .then(function (response) {
         console.log(response);
       })
